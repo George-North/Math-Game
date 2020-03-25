1c889055c5b8c149cac5e8822457f00b60cd262e
George-North <52474045+George-North@users.noreply.github.com>
2020-03-25 14:20:55 +1300

    Finished Homepage

--- 01 Skeletal Setup.py
+++ 01 Skeletal Setup.py
@@ -12,10 +12,46 @@ class main_window():
         self.menu.grid_columnconfigure(1,weight=1)
         Label(self.menu, text="George's Basic Facts", bg="#2c3f85", fg="white", font="Bahnschrift 25 bold").grid(row=0, column=0, columnspan=2, pady=15)
 
-        ttk.Button(self.menu, text="Start", command=lambda:basic_facts()).grid(row=1,column=0)
-        ttk.Button(self.menu, text="Highscores", command=lambda:highscores()).grid(row=1,column=1)
+        butt_style = ttk.Style()
+        butt_style.configure("big.TButton", font=("Arial", 25, "bold"))
 
+        drop_style = ttk.Style()
+        drop_style.configure("big.TMenubutton", font=("Arial", 10, "bold"))
+
+        check_style = ttk.Style()
+        check_style.configure("bg.TCheckbutton", background="#2c3f85", foreground="white", font=("Arial",11,"bold"))
         
+
+        self.Start_Button = ttk.Button(self.menu, text="Start", command=lambda:self.basic_facts(), width=7, style="big.TButton")
+        self.Start_Button.grid(row=1,column=0,pady=20)
+        ttk.Button(self.menu, text="Highscores", command=lambda:self.highscores()).grid(row=1,column=1,pady=20)
+
+        self.difficulty_value = StringVar()
+        self.difficulty = ttk.OptionMenu(self.menu, self.difficulty_value, "Difficulty","Easy","Medium","Hard","Expert", style="big.TMenubutton")
+        self.difficulty.config(width=10)
+        self.difficulty.grid(row=2,column=0,pady=20)
+
+        self.add_var = BooleanVar(value=True)
+        self.add = ttk.Checkbutton(self.menu, variable=self.add_var, command=self.an_option, width=13, style="bg.TCheckbutton", text="Addition")
+        self.add.grid(row=3,column=0,pady=20)
+        
+        self.sub_var = BooleanVar(value=True)
+        self.subtract = ttk.Checkbutton(self.menu, variable=self.sub_var, command=self.an_option, width=13, style="bg.TCheckbutton", text="Subtraction")
+        self.subtract.grid(row=4,column=0,pady=20)
+        
+        self.mult_var = BooleanVar(value=True)
+        self.multiply = ttk.Checkbutton(self.menu, variable=self.mult_var, command=self.an_option, width=13, style="bg.TCheckbutton", text="Multiplication")
+        self.multiply.grid(row=3,column=1,pady=20)
+        
+        self.div_var = BooleanVar(value=True)
+        self.divide = ttk.Checkbutton(self.menu, variable=self.div_var, command=self.an_option, width=13, style="bg.TCheckbutton", text="Division")
+        self.divide.grid(row=4,column=1,pady=20)
+
+    def an_option(self):
+        if self.add_var.get()==False and self.sub_var.get()==False and self.mult_var.get()==False and self.div_var.get()==False:
+            self.Start_Button.config(state=DISABLED)
+        else:
+            self.Start_Button.config(state=NORMAL)
         
         
     def basic_facts(self):
